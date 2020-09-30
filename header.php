18c1604864d50f27a2a85cd790fa2ce86202725d
Karan Sosa <karansosa1995@gmail.com>
2020-09-30 22:37:14 +0530

    slick added

--- header.php
+++ header.php
@@ -1,10 +1,37 @@
 <html>
 <head>
+
+<!-- bootstrap -->
 <link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- slick slider -->
+<link rel="stylesheet" href="css/slick.css">
+<!-- styles -->
+<link rel="stylesheet" href="css/style.css">
 </head>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Racing Events</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Schedule</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 </html>
 
